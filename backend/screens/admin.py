cb35eee80474bc4ed5707a7a7cb12fd941d4c6ed
Felipequesada21 <FELIPE.QUESADAPARADA@ucr.ac.cr>
2026-08-18 19:45:09 -0600

    refactor: remove unused screens app
    The screens app was removed. Real-time stop clients
    are served by the updates app's WebSocket topics instead.

--- backend/screens/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.gis import admin
-from .models import StopScreen, StationScreen, VehicleScreen
-
-# Register your models here.
-
-admin.site.register(StopScreen, admin.GISModelAdmin)
-admin.site.register(StationScreen, admin.GISModelAdmin)
-admin.site.register(VehicleScreen)
--- backend/screens/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("parada/<str:stop_slug>/", views.stop_screen, name="stop_screen"),
-]
